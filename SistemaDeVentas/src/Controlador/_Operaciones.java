8bcbb08590d794254b53d9ae8408777378efffd4
jgcamiloaga <jcamiloc148@gmail.com>
2024-11-27 10:53:53 -0500

    Se crea la interfaz _Operaciones con métodos abstractos para listar y eliminar elementos

---
+++ SistemaDeVentas/src/Controlador/_Operaciones.java
@@ -0,0 +1,10 @@
+package Controlador;
+
+import java.util.List;
+
+public interface _Operaciones {
+    
+    //METODOS ABSTRACTOS
+    public List listar();
+    public boolean eliminar(int id);
+}
